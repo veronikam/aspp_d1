3c177d1c94c0fcb907b9cd72e11338873a60fc81
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2014-09-09 13:47:03 +0200

    test 1 and 3 finished

--- carpentry_exercises/test_center.py
+++ carpentry_exercises/test_center.py
@@ -14,6 +14,50 @@ class FunctionalityTest(unittest.TestCase):
 		    returned = string.center(s, row[0])
 			#Then
 		    self.assertTrue(returned==row[1] or returned==row[2])
+	
+	def test_smaller(self):
+		#Given
+		s = 'abc'
+		width=2#len(s)-1
+		expected = s
+		#when
+		returned = string.center(s, width)
+		#Then
+		self.assertEqual(expected, returned)
+		self.assertLess(width,len(s))
+
+	def test_empty(self):
+	    #Given
+	    s = ''
+	    width=2
+	    expected = width*' '
+	    #when
+	    returned = string.center(s, width)
+	    #Then
+	    self.assertEqual(expected, returned)
+	    
+	def test_spaces(self):
+	    #Given
+	    s = ' abc '
+	    width=7
+	    expected = '  abc  '
+	    #when
+	    returned = string.center(s, width)
+	    #Then
+	    self.assertEqual(expected, returned)
+	    
+	def test_TypeError(self):
+		#Given
+		s = 'abc'
+		width = 7
+		list_fillchar = ['', 'ab']
+		
+		for fillch in list_fillchar:
+			#Then
+			with  self.assertRaises(TypeError):
+				#When
+				string.center(s, width, fillch)
+		
 
 if __name__ == '__main__': 
 	unittest.main()
